25322d3a9fd7b7df91c7fbca4cb4ac7274b92b6b
comicsansgreenkirby <benjamin1701a@gmail.com>
2022-08-03 01:01:08 -0500

    aaand delete it because bad root.

--- comicsansg-stuff/src/main/java/com/comicsansg/stuff/ComicsansgStuff.java
+++
@@ -1,9 +0,0 @@
-package com.comicsansg.stuff;
-import net.fabricmc.api.ModInitializer;
-
-public class ComicsansgStuff implements ModInitializer {
-    @Override
-    public void onInitialize() {
-
-    }
-}
