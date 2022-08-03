1723bd6b52b3786aa1b7c616f00a06fb2e608e9a
comicsansgreenkirby <benjamin1701a@gmail.com>
2022-08-03 01:01:54 -0500

    Initial files because I forgot to fork. (take 2)

---
+++ src/main/java/com/comicsansg/stuff/ComicsansgStuff.java
@@ -0,0 +1,9 @@
+package com.comicsansg.stuff;
+import net.fabricmc.api.ModInitializer;
+
+public class ComicsansgStuff implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
